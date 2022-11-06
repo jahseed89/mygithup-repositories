1bb77cce77cfd3ed75faecae61f2c3f0d63e2845
Samson Ocran <samson.ocran@gmail.com>
2022-11-06 16:10:46 +0100

    resulving conflict

--- src/navigations/PagesRoutes.js
+++ src/navigations/PagesRoutes.js
@@ -2,13 +2,6 @@ import React, { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import * as ContentMgtURL from "../urls/Routing";
 
-
-
-
-// const Home = lazy(() =>
-//   import("../pages/Home")
-// );
-
 const Repos = lazy(() =>
     import("../pages/Repos")
 );
