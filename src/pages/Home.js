c6bcd5d7db545a55047613b5a20c70405644511d
Samson Ocran <samson.ocran@gmail.com>
2022-11-14 10:44:35 +0100

    home page updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import * as ContentMgtURL from "../urls/Routing";
 import "./home.scss";
 import Button from "../components/button/Button";
 import BrandLoader from "../components/BrandLoader";
+import myAvatar from "../assets/images/samavatar.jpg";
 
 const Home = () => {
   const [userData, setUserData] = useState([]);
@@ -18,8 +19,8 @@ const Home = () => {
     axios
       .get(url)
       .then((response) => {
-        console.log(response.data.results);
-        setUserData(response.data.results);
+        console.log(response.data);
+        setUserData(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -37,11 +38,13 @@ const Home = () => {
   };
   return (
     <div className="container">
-      <Button
-        clickHandler={getUserRepo}
-        isActive={activeUser}
-        btnText="Get Repo"
-      />
+      <span>
+        <Button
+          clickHandler={getUserRepo}
+          isActive={activeUser}
+          btnText="Get Repo"
+        />
+      </span>
       <nav>
         <p>Logo</p>
         <ul>
@@ -51,30 +54,35 @@ const Home = () => {
         </ul>
         <div className="user-profile"></div>
       </nav>
+      <section className="landing-user">
+        <div className="user-profile">
+          <div className="user-pic">
+            <img src={myAvatar} alt="img" />
+          </div>
+          <p className="user-name">
+            <span>Samson Ocran</span>
+            <span>jahseed89</span>
+          </p>
+          <p className="user-bio">Web Developer#</p>
+        </div>
+      </section>
       {loading ? (
         <BrandLoader />
       ) : (
         <div className="user-wrapper">
           {userData.map((repo) => {
             return (
-              <div>
+              <div className="main" key={repo.owner.id}>
                 <div className="user-profile">
-                  <div className="user-pic"></div>
                   <p className="user-name">{repo.owner.login} </p>
-                  <p className="user-username">jahseed89</p>
-                  <p className="user-bio">A web Developer, ready to work</p>
                 </div>
-                <div className="user-repo">
-                  <input type="search" placeholder="Find a repositories..." />
-                  {repo.owner.url}
-                </div>
+                <Link onClick={handleClick} className="user-repo" to={repo.svn_url} >{repo.html_url}</Link>
               </div>
             );
           })}
         </div>
       )}
 
-      <Button clickHandler={handleClick} />
     </div>
   );
 };
