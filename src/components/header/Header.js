4de39bdaa9e8ed73c3683dd180f3a24cc350f8a2
Samson Ocran <samson.ocran@gmail.com>
2023-01-12 14:38:28 +0100

    header section updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { HOME_ROUTE, REPO_ROUTE } from "../../contents-management/Landing";
 import "./header.scss";
 
 
 const Header = () => {
+
+
   const navigate = useNavigate();
   const navigateToHome = () => {
     navigate(`/${HOME_ROUTE}`);
@@ -20,11 +21,10 @@ const Header = () => {
       <h2>Github App</h2>
       <ul>
         <li onClick={navigateToHome}>Home</li>
-        <li onClick={navigateToRepo} className="repo">Repositories<span>30</span></li>
-        <li>Projects</li>
+        <li onClick={navigateToRepo} className="repo">Repositories</li>
+        {/* <li>Projects</li>
         <li>Packages</li>
-        <li>Stars</li>
-        {/* <li>*888888</li> */}
+        <li>Stars</li> */}
       </ul>
     </nav>
   );
