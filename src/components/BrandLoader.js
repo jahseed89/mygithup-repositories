6614be0cc2fa0216176c91eacf0d59ed5cc87896
Samson Ocran <samson.ocran@gmail.com>
2023-03-13 14:17:01 +0100

    page updated

--- src/components/BrandLoader.js
+++ src/components/BrandLoader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BrandLoader = () => {
   return (
-    <h2>Loading....please wait!!!</h2>
+    <h2 style={{padding: "1rem"}}>Loading....please wait!!!</h2>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/button/Button";
 import BrandLoader from "../components/BrandLoader";
 import Header from "../components/header/Header";
 import { REPO_ROUTE } from "../contents-management/Landing";
-import MyContext from '../context'
+import MyContext from "../context";
 import "./home.scss";
 
 const Home = () => {
+  const { loading, userData } = useContext(MyContext);
+
   const navigate = useNavigate();
-  const { loading, userData } = useContext(MyContext)
 
   return (
     <div className="container">
@@ -18,18 +19,15 @@ const Home = () => {
         <BrandLoader />
       ) : (
         <section className="loading-profile">
-          {userData.map((repo) => {
+          {userData.slice(0, 1).map((data) => {
             return (
               <div className="user-profile">
-                <div className="user-pic" key={repo.id}>
-                  <img
-                    src={repo.owner.avatar_url}
-                    alt="#"
-                  />
+                <div className="user-pic" key={data.id}>
+                  <img src={data.owner.avatar_url} alt="#" />
                 </div>
                 <p className="user-name">
                   <span>Samson Ocran</span>
-                  <span>{repo.owner.login}</span>
+                  <span>{data.owner.login}</span>
                 </p>
                 <p className="user-bio">Web Developer#</p>
               </div>
