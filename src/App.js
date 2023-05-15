86e4681ad080d8d792ac01b3b4925363fd4451c1
Samson Ocran <samson.ocran@gmail.com>
2023-05-15 08:33:34 +0100

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import GlobalRoutes from '../src/navigations/GlobalRoute'
 const App = () => {
+  
   return (
     <BrowserRouter>
       <GlobalRoutes />
